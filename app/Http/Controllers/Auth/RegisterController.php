ed2f7c6e8a0dd61135ab06d45775bc7c8dc9292b
German Granados <ggranados@sunat.gob.pe>
2019-02-25 10:57:04 -0500

    feat: Agrega modulo autenticacion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:191'],
+            'email' => ['required', 'string', 'email', 'max:191', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'reg_token' => ['required', 'string', 'max:20'],
         ]);
     }
 
@@ -67,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'api_token' => str_random(60),
+            'reg_token' => $data['reg_token'],
         ]);
     }
 }
