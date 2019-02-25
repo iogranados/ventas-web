e51e998070edc4580aab83637234cb016814edf3
German Granados <ggranados@sunat.gob.pe>
2019-02-25 18:20:52 -0500

    fix: Agrega campos factory User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,5 +21,14 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
+        'api_token' => Str::random(60),
+        'reg_token' => Str::random(20),
+    ];
+});
+
+$factory->define(App\Seller::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'price' => $faker->randomDigit,
     ];
 });
