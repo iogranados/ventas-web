6e306b7a3b4a762782430c71012ef766724b1daf
German Granados <ggranados@sunat.gob.pe>
2019-02-27 15:41:02 -0500

    fix: Ajuste en decimales tabla products y payments

--- database/migrations/2019_02_26_195732_create_products_table.php
+++ database/migrations/2019_02_26_195732_create_products_table.php
@@ -13,38 +13,38 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('codproduct', 9)->unique();
             $table->string('name', 80);
-            $table->decimal('priceone', 8, 2);
-            $table->decimal('pricetwo', 8, 2);
-            $table->decimal('pricethree', 8, 2);
+            $table->decimal('priceone', 10, 3);
+            $table->decimal('pricetwo', 10, 3);
+            $table->decimal('pricethree', 10, 3);
             $table->integer('pricerangenameone');
-            $table->decimal('pricerangefromone', 4, 2);
-            $table->decimal('pricerangetoone', 4, 2);
-            $table->decimal('pricevaluefromone', 8, 2);
-            $table->decimal('pricevaluetoone', 8, 2);
+            $table->decimal('pricerangefromone', 6, 2);
+            $table->decimal('pricerangetoone', 6, 2);
+            $table->decimal('pricevaluefromone', 10, 3);
+            $table->decimal('pricevaluetoone', 10, 3);
             $table->integer('pricerangenametwo');
-            $table->decimal('pricerangefromtwo', 4, 2);
-            $table->decimal('pricerangetotwo', 4, 2);
-            $table->decimal('pricevaluefromtwo', 8, 2);
-            $table->decimal('pricevaluetotwo', 8, 2);
+            $table->decimal('pricerangefromtwo', 6, 2);
+            $table->decimal('pricerangetotwo', 6, 2);
+            $table->decimal('pricevaluefromtwo', 10, 3);
+            $table->decimal('pricevaluetotwo', 10, 3);
             $table->integer('pricerangenamethree');
-            $table->decimal('pricerangefromthree', 4, 2);
-            $table->decimal('pricerangetothree', 4, 2);
-            $table->decimal('pricevaluefromthree', 8, 2);
-            $table->decimal('pricevaluetothree', 8, 2);
+            $table->decimal('pricerangefromthree', 6, 2);
+            $table->decimal('pricerangetothree', 6, 2);
+            $table->decimal('pricevaluefromthree', 10, 3);
+            $table->decimal('pricevaluetothree', 10, 3);
             $table->integer('pricerangenamefour');
-            $table->decimal('pricerangefromfour', 4, 2);
-            $table->decimal('pricerangetofour', 4, 2);
-            $table->decimal('pricevaluefromfour', 8, 2);
-            $table->decimal('pricevaluetofour', 8, 2);
+            $table->decimal('pricerangefromfour', 6, 2);
+            $table->decimal('pricerangetofour', 6, 2);
+            $table->decimal('pricevaluefromfour', 10, 3);
+            $table->decimal('pricevaluetofour', 10, 3);
             $table->integer('pricerangenamefive');
-            $table->decimal('pricerangefromfive', 4, 2);
-            $table->decimal('pricerangetofive', 4, 2);
-            $table->decimal('pricevaluefromfive', 8, 2);
-            $table->decimal('pricevaluetofive', 8, 2);
+            $table->decimal('pricerangefromfive', 6, 2);
+            $table->decimal('pricerangetofive', 6, 2);
+            $table->decimal('pricevaluefromfive', 10, 3);
+            $table->decimal('pricevaluetofive', 10, 3);
             $table->integer('boxby');
             $table->string('typeunit', 1);
             $table->integer('priceoflist');
--- database/migrations/2019_02_26_212656_create_payments_table.php
+++ database/migrations/2019_02_26_212656_create_payments_table.php
@@ -29,8 +29,8 @@ class CreatePaymentsTable extends Migration
             $table->integer('NUMDOC')->nullable();
             $table->string('FECDOC', 8)->nullable();
             $table->string('MONEDA', 7)->nullable();
-            $table->decimal('MONTO', 10, 2)->nullable();
-            $table->decimal('CAMBIO', 6, 3)->nullable();
+            $table->decimal('MONTO', 10, 3)->nullable();
+            $table->decimal('CAMBIO', 8, 3)->nullable();
             $table->string('ANULADO', 1)->nullable();
             $table->bigInteger('ID_PAYMENT')->unique();
             $table->integer('IDORIGEN')->nullable();
